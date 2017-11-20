b5707fb0890cf238326d7921f3b38676397c7e1f
Pete Waters <pete.d.waters@gmail.com>
2017-11-20 20:51:25 +0000

    Default migrations and auth. Created models and migrations for sections and parts

---
+++ database/migrations/2017_11_20_204648_create_parts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('section_id')->unsigned();
+            $table->string('name');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+}
